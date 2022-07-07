ef0a72d4d72ab1df2857794aab686f47416e324d
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-07 09:58:17 -0300

    Finalizei os exercícios bonus do bloco 6.1

--- mod.01-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
+++ mod.01-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
@@ -1,13 +1,21 @@
 function enviar(event){
     event.preventDefault();
+    const validation = validationText();
+    if (validation === false){
+        alert('Dados Inválidos');
+    } else {
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+    }
 }
 
 window.onload = function() {
 
-const button = document.getElementsByClassName('btn')[0];
+const button = document.querySelector('#submit-btn');
 button.addEventListener('click', enviar);
-const clearButton = document.querySelector('clear-btn');
+const clearButton = document.querySelector('#clear-btn');
 clearButton.addEventListener('click', clear)
+const checkbox2 = document.querySelector('#checkbox2');
+checkbox2.addEventListener('change', habilitaButton);
 }
 
 const input = document.querySelectorAll('input');
@@ -20,4 +28,27 @@ function clear(){
 const textArea = document.querySelector('textarea');
 textArea.value = "";
 
+}
+
+function habilitaButton(){
+    const submitBtn = document.getElementById('submit-btn');
+    const checkbox2 = document.getElementById('checkbox2');
+    submitBtn.disabled = !checkbox2.checked;//Esse operador é chamado de "bang", e ele representa uma negação, ou seja, o contrário do retorno de alguma coisa. Se algo for verdadeiro ele retornará falso e vice-versa
+}
+
+function validationText (){
+    const getName = document.querySelector('#nome').value.length;
+    const checkName = getName < 10 || getName > 40;
+    
+    const getEmail = document.querySelector('#email').value.length;
+    const checkEmail = getEmail < 10 || getEmail > 50;
+
+    const getTextArea = document.querySelector('#resposta').value.length;
+    const checkTextArea = getTextArea > 500;
+
+    if(checkName || checkEmail || checkTextArea) {
+        return false;
+    } else {
+        return true;
+    }
 }
