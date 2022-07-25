eb475c36a37b2fc7a005619dfce9692385251602
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-25 00:09:07 -0300

    exercicio 6

---
+++ mod.01-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/hydrate.js
@@ -0,0 +1,40 @@
+// 🚀 6 - A função hydrate recebe uma string no formato "numero bebida", e retorna a sugestão de quantos copos de água você deve beber para se hidratar. Para cada bebida, deve-se tomar um copo de água para não ter ressaca. Exemplo:
+// // string recebida
+// '1 cerveja'
+// // retorno da função
+// '1 copo de água'
+
+// // string recebida
+// '1 cerveja, 2 shots e 1 catuaba'
+// // retorno da função
+// '4 copos de água'
+
+// // string recebida
+// '2 caipirinhas'
+// // retorno da função
+// '2 copos de água'
+// hydrate.js
+
+
+const hydrate = (string) => {
+    const splitString = string.split('');
+    let glassesOfWater = 0;
+  
+    for (let index = 0; index < splitString.length; index += 1) {
+      const parsedCharacter = parseInt(splitString[index]);
+  
+      if (parsedCharacter) {
+        glassesOfWater += parsedCharacter;
+      }
+    }
+  
+    let glass = 'copo';
+  
+    if (glassesOfWater > 1) {
+      glass = 'copos';
+    }
+  
+    return `${glassesOfWater} ${glass} de água`;
+  };
+  
+  module.exports = hydrate;
